571a2d07102a9469fc0b37ca0ab493008e16f1f5
ssomraaj <somraaj31@gmail.com>
2021-09-27 02:55:03 +0530

    added updated file

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const ethers = require("ethers");
 const ABI = require("./ABIs/NewABI");
 const fs = require('fs');
-const { InfuraKey, NewContract } = require("./config");
+const { InfuraKey, NewContract, OldContract } = require("./config");
 
 const provider = new ethers.providers.InfuraProvider("mainnet", InfuraKey);
 
 // Initialize the contract
 const contract = new ethers.Contract(NewContract, ABI, provider);
+const oldContract = new ethers.Contract(OldContract, ABI, provider);
 
 const owner = async (i) => {
     try {
@@ -38,4 +39,30 @@ const check = async () => {
     }
 }
 
-check();
+const update = async () => {
+    for(let i=0; i<587; i++) {
+        console.log(i);
+        let data = require("./result.json");
+        let result = data.result;
+
+        let query = result[i];
+        let res = await oldContract.ownerOf(query.id);
+        
+        let updated = require("./updated.json").updated;
+
+        const info = {
+            id: query.id,
+            owner: res
+        }
+
+        updated.push(info);
+
+        fs.writeFile("updated.json", JSON.stringify(updated), 'utf8', function(err) {
+            if(err) {
+                console.log(err);
+            }
+        })
+    }
+}
+
+update();
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     NewContract: "0x8ca5209d8cce34b0de91c2c4b4b14f20aff8ba23",
+    OldContract: "0x392179031da3012dac321703a29e4c9fbd26316b",
     InfuraKey: "b1edae4975f94f219f4de58f0ad61994"
 }
